59e7983256dbfc2ea07f0a25a08c35e5aedb4fd5
Joshua Lisoski <56094425+JLisoski@users.noreply.github.com>
2022-05-14 13:23:42 -0700

    Added Tutorial 3

---
+++ Tutorial3/Models/Model.cs
@@ -0,0 +1,107 @@
+﻿namespace MVVMTutorial3.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.ComponentModel;
+
+    class Model : IDataErrorInfo, INotifyPropertyChanged
+    {
+        private string customerName;
+
+        public string CustomerName
+        {
+            get
+            {
+                return customerName;
+            }
+
+            set
+            {
+                customerName = value;
+                OnPropertyChanged("CustomerName");
+            }
+        }
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region IDataErrorInfo Members
+
+        string IDataErrorInfo.Error
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        string IDataErrorInfo.this[string propertyName]
+        {
+            get
+            {
+                return GetValidationError(propertyName);
+            }
+        }
+
+        #endregion
+
+        #region Validation
+
+        static readonly string[] ValidatedProperties =
+{
+            "CustomerName"
+        };
+
+        public bool IsValid
+        {
+            get
+            {
+                foreach(string property in ValidatedProperties)
+                    if (GetValidationError(property) != null)
+                        return false;
+
+                return true;
+            }
+        }
+
+        string GetValidationError(String propertyName)
+        {
+            string error = null;
+
+            switch (propertyName)
+            {
+                case "CustomerName":
+                    error = ValidateCustomerName();
+                    break;
+            }
+
+            return error;
+        }
+        private string ValidateCustomerName()
+        {
+            if (String.IsNullOrWhiteSpace(CustomerName))
+            {
+                return "Customer name cannot be empty.";
+            }
+
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ Tutorial3/ViewModels/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿namespace MVVMTutorial3.ViewModels
+{
+    using MVVMTutorial3.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel()
+        {
+            Model = new Model()
+            {
+                CustomerName = "Joshua"
+            };
+        }
+        public Model Model
+        {
+            get;
+            set;
+        }
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
