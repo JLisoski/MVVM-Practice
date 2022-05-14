26d9c06aabe4b310301750bd7e0df794d0b2c432
Joshua Lisoski <56094425+JLisoski@users.noreply.github.com>
2022-05-13 22:24:30 -0700

    Add project files.

---
+++ Commands/CustomerUpdateCommand.cs
@@ -0,0 +1,38 @@
+﻿namespace MVVMTutorial.Commands
+{
+    using System.Windows.Input;
+    using MVVMTutorial.ViewModels;
+
+    internal class CustomerUpdateCommand : ICommand
+    {
+        /// <summary>
+        /// Initialize a new instance of the CustomerUpdateCommand Class.
+        /// </summary>
+        public CustomerUpdateCommand(CustomerViewModel viewModel)
+        {
+            _ViewModel = viewModel;
+        }
+
+        private CustomerViewModel _ViewModel;
+
+        #region ICommand Members
+
+        public event System.EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value;}
+        }
+
+        public bool CanExecute(object paramater)
+        {
+            return _ViewModel.CanUpdate;
+        }
+
+        public void Execute(object paramater)
+        {
+            _ViewModel.SaveChanges();
+        }
+
+        #endregion
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace MVVMTutorial.Models
+{
+    using System;
+    using System.ComponentModel;
+    public class Customer : INotifyPropertyChanged
+    {
+        /// <summary>
+        ///  Initializes a new instance of the Customer Class.
+        /// </summary>
+        public Customer(String customerName)
+        {
+            Name = customerName;
+        }
+
+        private String _Name;
+        /// <summary>
+        /// Gets or sets the Customer's name. 
+        /// </summary>
+        public String Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+
+        #endregion
+    }
+}
